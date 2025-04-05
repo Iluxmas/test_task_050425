298e12520cf4f8e89bc6f8a368bbb54d5fc37b5d
Sulkhanov Ilya <masta.pxty@gmail.com>
2025-04-05 16:36:46 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/test_task_050425/',
   server: {
     host: true,
     port: 5656
